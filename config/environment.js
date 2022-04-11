c09baeb01bafd6fced847d327a14a52a2f8ed193
hkrai-tech <8697422699hk@gmail.com>
2022-04-11 22:09:16 +0530

    Final Commit

--- config/environment.js
+++ config/environment.js
@@ -9,8 +9,8 @@ const development = {
     smtp:{
         service:"gmail",
         host: "smtp.gmail.com",
-        port: 25,
-        secure: false, // true for 465, false for other ports
+        port: 465,
+        secure: true, // true for 465, false for other ports
         auth: {
           user: process.env.user, 
           pass: process.env.pass,
