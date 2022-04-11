193cfe0011552b13cf2cdb91c731fbcd724bd9ee
hkrai-tech <8697422699hk@gmail.com>
2022-04-11 22:49:12 +0530

    Final Commit

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cors({
 }))
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://54.226.206.106:4000");
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
